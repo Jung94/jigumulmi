5a5a3c220d1d87017b01c11dbc1918d1f7f4bd23
SungMokJung <newyear94@naver.com>
2024-02-21 23:44:03 +0900

    Feat: registration

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -7,6 +7,7 @@ import SearchBar from '@/components/searchBar'
 
 const Header = () => {
   const windowSize = useWindowSize()
+  console.log(windowSize.width)
 
   return (
     <div className={styles.container}>
@@ -15,6 +16,13 @@ const Header = () => {
           <div className={styles.links}>
             <div className={styles.logo}>JIGUMULMI</div>
           </div>
+
+          {/* Mobile */}
+          {windowSize.width <= 1100 &&
+            <div className={styles.links}>
+              <div className={styles.registration}>등록하기</div>
+            </div>
+          }
 
           {/* PC */}
           {1100 < windowSize.width &&
