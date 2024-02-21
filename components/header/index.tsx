1805a0c8c6f27a64f53773571914ca7c34a69f4c
SungMokJung <newyear94@naver.com>
2024-02-22 00:06:23 +0900

    Feat: registration

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -13,15 +13,19 @@ const Header = () => {
     <div className={styles.container}>
       <header>
         <nav className={styles.nav}>
-          <div className={styles.links}>
+          <button className={`${styles.links} ${styles.logo}`}>
             <div className={styles.logo}>JIGUMULMI</div>
-          </div>
+          </button>
 
           {/* Mobile */}
           {windowSize.width <= 1100 &&
-            <div className={styles.links}>
-              <div className={styles.registration}>등록하기</div>
-            </div>
+            <button className={`${styles.links} ${styles.registration}`}>
+              <div>등록하기</div>
+              {/* <svg width="25px" height="25px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+                <path d="M8 12H12M16 12H12M12 12V8M12 12V16" stroke="#000000" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"></path>
+                <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#000000" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"></path>
+              </svg> */}
+            </button>
           }
 
           {/* PC */}
