a4a80e64e54a447d92d8cc6c9bb97489014697fc
SungMokJung <newyear94@naver.com>
2024-02-21 23:24:20 +0900

    Fix: remove Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <Providers>
           <Header />
           {children}
-          <Navbar />
+          {/* <Navbar /> */}
         </Providers>
       </body>
     </html>
