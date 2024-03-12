4f296fb5666402827fd255f28f135189e6894a1b
SungMokJung <newyear94@naver.com>
2024-03-12 23:19:01 +0900

    fix: add style prop

--- lib/hooks/useModal.tsx
+++ lib/hooks/useModal.tsx
@@ -3,11 +3,11 @@
 import { useRef, useEffect } from 'react';
 import Dialog from '@/components/modal/Dialog';
 
-const useModal = (component: React.ReactNode) => {
+const useModal = (component: React.ReactNode, style?: any) => {
   const dialogRef = useRef<HTMLDialogElement | null>(null);
 
   // forwardRef로 컴포넌트 분리 필요
-  const create = () => <Dialog ref={dialogRef} component={component} />;
+  const create = () => <Dialog ref={dialogRef} component={component} style={style} />;
 
   const open = () => {
     dialogRef.current?.showModal();
