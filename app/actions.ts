2942ed3e62f142b013ce1537af1ef68ca3cc18cd
SungMokJung <newyear94@naver.com>
2024-05-26 15:37:18 +0900

    chore

--- app/actions.ts
+++ app/actions.ts
@@ -5,11 +5,7 @@ import { APIsearch } from '@/lib/api/search'
 import { convertPlaceList } from '@/app/search/_utils/convertPlaceList'
 
 export async function getPlaceList() {
-  // const res = await fetch()
-  const res = await getAPI(
-    APIsearch.getPlaceList,
-    { subwayStationId: null, placeId: null }
-  )
+  const res = await getAPI(APIsearch.getPlaceList, { subwayStationId: null, placeId: null })
   
   if (res.status !== 200) throw new Error('Failed: getPlaceList')
 
