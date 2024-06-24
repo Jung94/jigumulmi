120da3f7ee5876c0f9725086963691d7b0415523
SungMokJung <newyear94@naver.com>
2024-06-25 00:06:51 +0900

    fix: link in home page

--- components/_pages/home/components/sections/05/index.tsx
+++ components/_pages/home/components/sections/05/index.tsx
@@ -12,9 +12,12 @@ const Section05 = () => {
           지구에게 편안한 속도를 찾아가는 여정, 지구멀미 팀의 이야기를 확인해 보세요.
         </div>
         <div className={styles['section-inner-button-wrapper']}>
+          
+        <Link href="https://developerjoseph.notion.site/dfea728380654c84b4b898b63e411707?pvs=4" target='_blank'>
           <button className={styles['section-inner-button']}>
-            <Link href="https://developerjoseph.notion.site/dfea728380654c84b4b898b63e411707?pvs=4" target='_blank'>팀 소개 바로가기</Link>
+          팀 소개 바로가기
           </button>
+        </Link>
         </div>
       </div>
     </div>
--- components/_pages/home/components/sections/06/index.tsx
+++ components/_pages/home/components/sections/06/index.tsx
@@ -9,9 +9,11 @@ const Section06 = () => {
           지구멀미가 더 나은 정보를 제공할 수 있도록 <br/>개선 의견을 남겨주시면 적극적으로 반영하겠습니다.
         </div>
         <div className={styles['section-inner-button-wrapper']}>
-          <button className={styles['section-inner-button']}>
-            <Link href="https://smore.im/form/gKNbMW1VUf" target='_blank'>의견 남기기</Link>
-          </button>
+          <Link href="https://smore.im/form/gKNbMW1VUf" target='_blank'>
+            <button className={styles['section-inner-button']}>
+              의견 남기기
+            </button>
+          </Link>
         </div>
       </div>
     </div>
