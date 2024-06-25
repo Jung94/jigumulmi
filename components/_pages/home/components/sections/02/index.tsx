57283555b1f745eb0fc03d9cc1a0fd9f00ae9a33
SungMokJung <newyear94@naver.com>
2024-06-25 21:33:58 +0900

    fix: change image and add user icon

--- components/_pages/home/components/sections/02/index.tsx
+++ components/_pages/home/components/sections/02/index.tsx
@@ -21,7 +21,12 @@ const Section02 = () => {
         <div className={styles['section-inner-image']}>
           <div className={styles['section-inner-image-header']}>
             <div className={styles['section-inner-image-header-left']}>지구멀미</div>
-            <div className={styles['section-inner-image-header-right']}></div>
+            <div className={styles['section-inner-image-header-right']}>
+              <svg width="21px" height="21px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+                <path d="M5 20V19C5 15.134 8.13401 12 12 12V12C15.866 12 19 15.134 19 19V20" stroke="#666" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
+                <path d="M12 12C14.2091 12 16 10.2091 16 8C16 5.79086 14.2091 4 12 4C9.79086 4 8 5.79086 8 8C8 10.2091 9.79086 12 12 12Z" stroke="#666" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"></path>
+              </svg>
+            </div>
           </div>
           {649 < windowSize.width
             ? <Image className={styles['section-inner-image-content']} src={Section02PcImage} width={440} height={108} placeholder='blur' sizes="440px" layout='fixed' alt='section-02-pc'></Image>
