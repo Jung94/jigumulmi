524e4fe69a25b0919c80cf7142822b458710fbe7
SungMokJung <newyear94@naver.com>
2024-11-01 09:11:23 +0900

    design: 별 기본 색상 변경

--- components/modal/registration-review/Content.tsx
+++ components/modal/registration-review/Content.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import styles from './registration-review.module.scss';
 import { useSearchParams } from 'next/navigation';
 import Button from '@/components/button';
+import UploadingImage from './uploading-image';
 import { useModal } from '@/lib/hooks';
 import SuccessContent from '@/components/modal/success/Content';
 import RequestLoginContent from '@/components/modal/request-login/Content';
@@ -17,7 +18,7 @@ import { APIreview } from "@/lib/api/review";
 const Star = ({ active, onClick }: { active: boolean, onClick: ()=>void }) => {
   return (
     <button type='button' className={styles["star-button"]} onClick={onClick}>
-      <svg width="40px" height="40px" strokeWidth="1.5" viewBox="0 0 24 24" fill={active ? '#0060AE' : 'hsl(0,0%,85%)'} xmlns="http://www.w3.org/2000/svg" color="#000000">
+      <svg width="40px" height="40px" strokeWidth="1.5" viewBox="0 0 24 24" fill={active ? '#0060AE' : '#e5e8eb'} xmlns="http://www.w3.org/2000/svg" color="#000000">
         <path d="M8.58737 8.23597L11.1849 3.00376C11.5183 2.33208 12.4817 2.33208 12.8151 3.00376L15.4126 8.23597L21.2215 9.08017C21.9668 9.18848 22.2638 10.0994 21.7243 10.6219L17.5217 14.6918L18.5135 20.4414C18.6409 21.1798 17.8614 21.7428 17.1945 21.3941L12 18.678L6.80547 21.3941C6.1386 21.7428 5.35909 21.1798 5.48645 20.4414L6.47825 14.6918L2.27575 10.6219C1.73617 10.0994 2.03322 9.18848 2.77852 9.08017L8.58737 8.23597Z" stroke="#000000" strokeWidth="0" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
     </button>
@@ -186,6 +187,7 @@ const RegistrationReviewContent = ({
           <div className={styles.title}>
             리뷰
           </div>
+          <UploadingImage />
           <textarea id='review' name='review' maxLength={400} placeholder={placeholder} value={review} onChange={handleReview} />
         </div>
 
