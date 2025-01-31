a733352c99477b05696da55b77881ab012a51568
SungMokJung <newyear94@naver.com>
2025-02-01 01:50:03 +0900

    chore: 코드 정리

--- src/2.widgets/admin-place/place-form/business-hour-section.tsx
+++ src/2.widgets/admin-place/place-form/business-hour-section.tsx
@@ -1,13 +1,9 @@
 'use client'
 
-import { useParams } from 'next/navigation'
+import { Form } from '@/src/shared/ui/admin'
 import { Dispatch, SetStateAction } from 'react'
-import { useQueryClient } from '@tanstack/react-query'
 import FixedBusinessHourForm from './fixed-business-hour-form'
-import { Form, Button, CalendarBase } from '@/src/shared/ui/admin'
 import TemporaryBusinessHourForm from './temporary-business-hour-form'
-import placeQueryKey from '@/src/4.entities/place-admin/model/queries/query-key.constant'
-import { useUpdatePlaceMenu } from '@/src/4.entities/place-admin/model/queries'
 import type { PlaceBusinessHour } from '@/src/4.entities/place-admin/model/types'
 
 export default function BusinessHourSection(props: {
@@ -15,13 +11,6 @@ export default function BusinessHourSection(props: {
   setBusinessHourData: Dispatch<SetStateAction<PlaceBusinessHour>>
   setMonth: Dispatch<SetStateAction<number>>
 }) {
-  const params = useParams()
-  const queryClient = useQueryClient()
-  const updatePlaceMenu = useUpdatePlaceMenu()
-  const placeId = params?.placeId ? Number(params.placeId) : null
-
-  const handleUpdate = async () => {}
-
   return (
     <Form>
       <FixedBusinessHourForm {...props} />
