473a77883bae0745dfd199c5d9fa2ad3226f3aa4
SungMokJung <newyear94@naver.com>
2025-02-11 23:15:10 +0900

    delete: 로딩 스피너 컴포넌트, scss 삭제

--- components/icon/loading-spinner/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './loading-spinner.module.scss';
-
-const LoadingSpinner = () => {
-  return (
-    <div className={styles["lds-spinner"]}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-  );
-}
-
-export default LoadingSpinner;
