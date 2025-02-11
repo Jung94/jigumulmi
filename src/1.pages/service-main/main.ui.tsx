ee267325211b7de9b5ae313488aaae49704e7ea4
SungMokJung <newyear94@naver.com>
2025-02-11 23:40:05 +0900

    design: padding, height, 아이콘 fill 수정

--- src/1.pages/service-main/main.ui.tsx
+++ src/1.pages/service-main/main.ui.tsx
@@ -14,7 +14,7 @@ export default function MainPage() {
 
   const drawArrowIcon = () => {
     return (
-      <svg width="19px" height="19px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+      <svg width="19px" height="19px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M6.00005 19L19 5.99996M19 5.99996V18.48M19 5.99996H6.52005" stroke="#444" strokeWidth="1.85" strokeLinecap="round" strokeLinejoin="round"></path>
       </svg>
     )
