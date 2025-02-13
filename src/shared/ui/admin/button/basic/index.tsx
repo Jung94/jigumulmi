586b31ec6c057ced6350640aa377605201f1005f
SungMokJung <newyear94@naver.com>
2025-02-14 02:19:17 +0900

    fix: LoadingSpinner 변경

--- src/shared/ui/admin/button/basic/index.tsx
+++ src/shared/ui/admin/button/basic/index.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import { ReactNode, MouseEvent } from 'react'
-import { useRouter } from 'next/navigation'
 import styles from './basic.module.scss'
-import Image from 'next/image'
-import SpinnerBlack from '@/public/icons/loading-spinner.svg'
-import SpinnerWhite from '@/public/icons/loading-spinner-white.svg'
+import { useRouter } from 'next/navigation'
+import { ReactNode, MouseEvent } from 'react'
+import { LoadingSpinner } from '@/src/shared/assets/icons'
 
 type Props = {
   style?: any
@@ -44,7 +42,7 @@ export default function Button({
   }
 
   const setLoading = () => {
-    return <Image src={variant === 'filled' ? SpinnerWhite : SpinnerBlack} width={25} height={25} alt='loading-spinner' />
+    return <LoadingSpinner width={20} height={20} />
   }
 
   const Content = () => (
