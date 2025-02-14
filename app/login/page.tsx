e71528c1561d42b7eb6d926150ba90949499c5ab
SungMokJung <newyear94@naver.com>
2025-02-14 17:12:48 +0900

    fix: suspense 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react'
 import LoginPage from '@/src/1.pages/login'
 
 export default function Page() {
-  return <LoginPage />
+  return (
+    <Suspense>
+      <LoginPage />
+    </Suspense>
+  )
 }
