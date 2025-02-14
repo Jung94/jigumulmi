46f90eb4212fd2dd894609626c9cc1269ee24bdc
SungMokJung <newyear94@naver.com>
2025-02-14 17:04:30 +0900

    delete: fsd 폴더구조 정리

--- app/login/actions.ts
+++
@@ -1,7 +0,0 @@
-import { postAPI } from "@/lib/api"
-import { APIaccount } from '@/lib/api/account'
-
-export async function checkRegistered(code: string, redirectUrl: string) { // 회원가입된 유저인지 아닌지
-  const response = await postAPI({apiURL: APIaccount.checkRegisteredUser, body: { code, redirectUrl }})
-  return response
-}
