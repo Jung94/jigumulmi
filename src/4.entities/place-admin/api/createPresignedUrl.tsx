bba10fe0e0c5cfe24b2ea067484c7917bdbaf1dd
SungMokJung <newyear94@naver.com>
2025-02-15 21:56:11 +0900

    refactor: endpoint 수정

--- src/4.entities/place-admin/api/createPresignedUrl.tsx
+++ src/4.entities/place-admin/api/createPresignedUrl.tsx
@@ -1,11 +1,11 @@
 import { apiClient } from '@/src/shared/api/fetch'
-import { placeAPI } from './place.constant'
+import { placeAmdinAPI } from './place.constant'
 
 export default async function createPresignedUrl(
   body: { fileExtension: string }
 ) {
   return await apiClient.post({
-    endpoint: `${placeAPI.putPresignedUrl}`,
+    endpoint: `${placeAmdinAPI.putPresignedUrl}`,
     body
   })
 }
